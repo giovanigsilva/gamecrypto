94c0fedcf9a3fc22c54e06d4ae1a953be1c9d08c
Giovani G <giovanigsilva@gmail.com>
2022-02-13 04:09:45 -0300

    Update 1155nft.sol

--- 1155nft.sol
+++ 1155nft.sol
@@ -4,14 +4,18 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract mygameNFT is ERC1155, Ownable {
+    string public name;
+    string public symbol;
     uint256 public tokensInCirculation;
 
     constructor() ERC1155("") {
+    	name = "ETEST";
+	symbol = "ET";
         tokensInCirculation = 1;
     }
 
     function mint(uint256 amount) public onlyOwner {
-        for(uint i = -; i < amount; i++) {
+        for(uint i = 0; i < amount; i++) {
 		_mint(msg.sender, tokensInCirculation, 1, "");
 		tokensInCirculation++;
     	}
