d0ffad0faa446cd5ec2508658dafe9de8796f2b3
Giovani G <giovanigsilva@gmail.com>
2022-02-15 23:25:27 -0300

    Add files via upload

---
+++ YourToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.4;
+
+// Learn more about the ERC20 implementation 
+// on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/erc20
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract YourToken is ERC20 {
+    constructor() ERC20("Scaffold BNB Token", "SET") {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
