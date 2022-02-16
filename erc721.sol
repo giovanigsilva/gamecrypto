6597010b29f857403e6f130859e03e097cb50078
Giovani G <giovanigsilva@gmail.com>
2022-02-15 23:41:48 -0300

    Update erc721.sol

--- erc721.sol
+++ erc721.sol
@@ -3,14 +3,20 @@ pragma solidity 0.8.11;
  
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+import "./YourToken.sol";
  
 contract newNFT is NFTokenMetadata, Ownable {
- 
+  uint256 public tokensPerBNB = 100;
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
+  YourToken yourToken;
   
-  constructor() {
+  constructor(address tokenAddress) {
     nftName = "ENERGY NFT";
     nftSymbol = "ENECF";
     nextTokenId = 0;
+    yourToken = YourToken(tokenAddress);
+
   }
  
  //Scores dos jogadores
@@ -76,6 +82,57 @@ contract newNFT is NFTokenMetadata, Ownable {
  
   function getTokensIdsList() public view returns (tokensIdOfOwner[] memory) {
     return tokensIdsList;
+  }
+
+  function buyTokens() public payable returns (uint256 tokenAmount) {
+    require(msg.value > 0, "Send ETH to buy some tokens");
+
+    uint256 amountToBuy = msg.value * tokensPerBNB;
+
+    // check if the Vendor Contract has enough amount of tokens for the transaction
+    uint256 vendorBalance = yourToken.balanceOf(address(this));
+    require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
+
+    // Transfer token to the msg.sender
+    (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
+    require(sent, "Failed to transfer token to user");
+
+    // emit the event
+    emit BuyTokens(msg.sender, msg.value, amountToBuy);
+
+    return amountToBuy;
+  }
+
+  /**
+  * @notice Allow users to sell tokens for ETH
+  */
+    function sellTokens(uint256 tokenAmountToSell) public {
+    // Check that the requested amount of tokens to sell is more than 0
+    require(tokenAmountToSell > 0, "Specify an amount of token greater than zero");
+
+    // Check that the user's token balance is enough to do the swap
+    uint256 userBalance = yourToken.balanceOf(msg.sender);
+    require(userBalance >= tokenAmountToSell, "Your balance is lower than the amount of tokens you want to sell");
+
+    // Check that the Vendor's balance is enough to do the swap
+    uint256 amountOfETHToTransfer = tokenAmountToSell / tokensPerBNB;
+    uint256 ownerETHBalance = address(this).balance;
+    require(ownerETHBalance >= amountOfETHToTransfer, "Vendor has not enough funds to accept the sell request");
+
+    (bool sent) = yourToken.transferFrom(msg.sender, address(this), tokenAmountToSell);
+    require(sent, "Failed to transfer tokens from user to vendor");
+
+
+    (sent,) = msg.sender.call{value: amountOfETHToTransfer}("");
+    require(sent, "Failed to send BNB to the user");
+  }
+
+  function withdraw() public onlyOwner {
+    uint256 ownerBalance = address(this).balance;
+    require(ownerBalance > 0, "Owner has not balance to withdraw");
+
+    (bool sent,) = msg.sender.call{value: address(this).balance}("");
+    require(sent, "Failed to send user balance back to the owner");
   }
   
 }
