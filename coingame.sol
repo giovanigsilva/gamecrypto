cfccca2e895d89af0399e7a25ec62cf76008b268
Giovani G <giovanigsilva@gmail.com>
2022-02-16 17:10:06 -0300

    Update coingame.sol

--- coingame.sol
+++ coingame.sol
@@ -3,28 +3,24 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract tokenGame is Ownable{
+contract tokenGame{
 
     
-    mapping(address => uint) public balances;
-    mapping(address => mapping(address => uint)) public allowance;
-    
+    mapping(address => uint256) internal balances;
+       
     string public name = "EnergyCoin";
     string public symbol = "ENEC";
 
-    uint public numeroDeMoedas = 21000000;
-    uint public casasDecimais = 8;
+    uint private numeroDeMoedas = 21000000;
+    uint private casasDecimais = 8;
     
-    event Transfer(address indexed from, address indexed to, uint value);
+    event Transfer(address indexed from, address indexed to, uint);
     
     uint public totalSupply = numeroDeMoedas * 10 ** casasDecimais;
     uint public decimals = casasDecimais;
     
-    address public contractOwner;
-
-
-  
-    
+    address private contractOwner;
+ 
     constructor() {
         contractOwner = msg.sender;
         balances[msg.sender] = totalSupply;
@@ -34,23 +30,31 @@ contract tokenGame is Ownable{
         return balances[owner];
     }
     
-    
-    function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);  
     }
 
-    function _burn(address account, uint256 amount) internal returns(bool){
+    function _burn(address account, uint amount) internal returns(bool){
         require(amount != 0);
         require(amount <= balances[account]);
         totalSupply -= amount;
         balances[account] -= amount;
-        emit Transfer(account, address(0), amount);
+        emit Transfer(account, address(0), amount);  
         return true;
     }
 
-    
-    function createTokens(uint value) public returns(bool) {
+    function transfer(address account, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[msg.sender], "Not funds");
+        balances[msg.sender] -=  numTokens;
+        balances[account] += numTokens;
+        emit Transfer(msg.sender, account, numTokens);
+        return true;
+    }
+
+    function createTokens(uint value) internal returns(bool) {
+        
         if(msg.sender == contractOwner) {
+            require(value != 0, "not zero permission");
             totalSupply += value;
     	    balances[msg.sender] += value;
     	    return true;
@@ -58,14 +62,28 @@ contract tokenGame is Ownable{
         return false;
     }
 
-    function destroyTokens(uint value) public returns(bool) {
+    function destroyTokens(uint value) internal returns(bool) {
         if(msg.sender == contractOwner) {
-            require(balanceOf(msg.sender) >= value, 'Saldo insuficiente (balance too low)');
+            require(value != 0, "not zero permission");
+            require(balanceOf(msg.sender) >= value, "balance too low");
             totalSupply -= value;        
     	    balances[msg.sender] -= value;
             return true;
         }
         return false;
+    }
+
+    function mint(uint amount) external {
+        _mint(msg.sender, amount);  
+    }
+
+    function _mint(address account, uint amount) internal virtual returns(bool) {
+        require(account != address(0), "ERC20: mint to the zero address");
+        require(amount < 0, "Amount not valid");
+        totalSupply += amount;
+        balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+        return true;
     }
     
 }
