7644c937bd361c0e3d208b9291cf110e21f02588
Giovani G <giovanigsilva@gmail.com>
2022-02-16 21:34:40 -0300

    Update erc721Game.sol

--- erc721Game.sol
+++ erc721Game.sol
@@ -2,29 +2,27 @@
 pragma solidity 0.8.11;
  
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
 contract newNFT is NFTokenMetadata{
+    uint public decimals = 2;
   
   constructor() {
     nftName = "ENERGY NFT";
-    nftSymbol = "ENECF";
+    nftSymbol = "ENEFT";
     nextTokenId = 0;
     }
  
-  uint public nextTokenId;
-  tokensIdOfOwner[] public tokensIdsList;
-  
+  uint private nextTokenId;
+
   function mint(string calldata uri) external {
     _mintage(msg.sender, uri);  
   }
 
-  function _mintage(address _to, string calldata _uri) internal{
+  function _mintage(address _to, string calldata _uri) internal returns(uint){
     nextTokenId++;
     super._mint(_to, nextTokenId);
     super._setTokenUri(nextTokenId, _uri);
-    updateTokensIdList(_to, nextTokenId);
-    
+    return nextTokenId;
   }  
   
 }
